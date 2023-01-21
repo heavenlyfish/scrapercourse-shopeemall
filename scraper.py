c3dbc2d7117418ce10305bc261052a4a9d7819a7
heavenlyfish <42778096+heavenlyfish@users.noreply.github.com>
2023-01-21 22:59:48 +0800

    5.3 批次爬取動態網頁的資料

---
+++ scraper.py
@@ -0,0 +1,23 @@
+#2023/01/21 Code not working, moving forward for now
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get('https://shopee.tw/%E5%B1%85%E5%AE%B6%E7%94%9F%E6%B4%BB-cat.11040925')
+time.sleep(5)
+#there are better ways to pause and wait for website to load
+
+cards = driver.find_elements(By.CSS_SELECTOR, "div[class='col-xs-2-4 shopee-search-item-result__item']")
+
+items = []
+for card in cards:
+    title = card.find_element(By.CSS_SELECTOR, "div[class='_1yN94N WoKSjC _2KkMCe']").text
+    price = card.find_element(By.CSS_SELECTOR, "div[class='cbl0HO MUmBjS']").text
+    link = card.find_element(By.TAG_NAME, "a").get_attribute('href')
+    items.append((title,price,link))
+
+print(items)
+
